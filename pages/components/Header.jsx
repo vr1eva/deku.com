01e3a684a5931196f5bdc65e4e94f7745996f4f5
vr1 <113930888+vr1eva@users.noreply.github.com>
2023-01-30 19:20:22 +0000

    render links in header

---
+++ pages/components/Header.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import Link from "next/link"
+import logo from "../assets/logo.svg"
+
+const links = [
+    {name: "Arte", href:"/arte"},
+    {name: "Ropa", href:"/ropa"},
+    {name: "Accesorios", href:"/accesorios"},
+    {name: "Hogar", href:"/hogar"},
+    {name: "Marcas", href:"/marcas"},
+    {name: "Más", href:"/mas"},
+]
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <Image src={logo} width={80} height={21} />
+            <nav>
+                {links.map(link => (
+                    <Link href={link.href}>{link.name}</Link>
+                ))}
+            </nav>
+        </div>
+
+    )
+}
