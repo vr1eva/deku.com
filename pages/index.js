4588ec7ecece745351a21761c561114ce731aac0
vr1 <113930888+vr1eva@users.noreply.github.com>
2023-01-31 03:18:58 +0000

    import missing component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Explore from "./components/Explore"
 
 export default function Home() {
   return (
